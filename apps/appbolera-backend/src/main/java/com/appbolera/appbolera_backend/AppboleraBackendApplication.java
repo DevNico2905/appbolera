139a86125cad782071e7c69d5470fe840e85af2b
Nicolas Bernal <118196985+DevNico2905@users.noreply.github.com>
2025-09-08 13:31:12 -0500

    Set up Backend

---
+++ apps/appbolera-backend/src/main/java/com/appbolera/appbolera_backend/AppboleraBackendApplication.java
@@ -0,0 +1,13 @@
+package com.appbolera.appbolera_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppboleraBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppboleraBackendApplication.class, args);
+	}
+
+}
---
+++ apps/appbolera-backend/src/test/java/com/appbolera/appbolera_backend/AppboleraBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.appbolera.appbolera_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppboleraBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
