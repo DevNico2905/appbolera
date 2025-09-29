a220af31e9b6733994bca952bf12e208b96dfd2c
Nicolas Bernal <118196985+DevNico2905@users.noreply.github.com>
2025-09-29 17:04:37 -0500

    Details

--- apps/appbolera-frontend/src/app/services/player/player.service.ts
+++ apps/appbolera-frontend/src/app/services/player/player.service.ts
@@ -1,8 +1,27 @@
-import { Injectable } from '@angular/core';
+import { Inject, Injectable } from '@angular/core';
+import { environment} from '../../environment/environment.development';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+interface Player{
+  id?: number;
+  name: string;
+  gameId: string;
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class PlayerService {
-  
+
+  // "/saveGameId/{gameId}"
+  // "/savePlayers"
+
+  private http = Inject(HttpClient);
+
+  // Endpoint guardar jugadores.
+  savePlayers(players: Player[]): Observable<Player[]> {
+    return this.http.post<Player[]>(`${environment.backendHost}/savePlayers`, players);
+  }
+
 }
